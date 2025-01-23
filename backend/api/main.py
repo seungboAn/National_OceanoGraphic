7d3a5952a75fcb4a0c6bc9f2d4701d837c46e04b
seungboAn <seungbo1112@gmail.com>
2025-01-23 12:38:58 +0900

    fix: Modified CORS

--- backend/api/main.py
+++ backend/api/main.py
@@ -8,8 +8,16 @@ import io
 import numpy as np
 import time
 from pyngrok import ngrok
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 출처 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 class ModelOption(str, Enum):
     fast = "fast"
